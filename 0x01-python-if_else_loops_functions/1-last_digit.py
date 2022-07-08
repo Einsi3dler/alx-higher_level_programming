e13ad1712bbfd001312e174e112305942ed1a626
Sylvester_Divine <sylvesterdivine@outlook.com>
2022-07-08 23:57:49 +0000

    task one, hotfix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number == 0:
-    print(f"Last digit of{number} is {lst} and is 0")
+    print(f"Last digit of {number} is {lst} and is 0")
 if number < 0:
     lst = number % -10
 if number > 0:
