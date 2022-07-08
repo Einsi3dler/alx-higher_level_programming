6edacb7ada33842e7ef0c5109634151a893e1945
Sylvester_Divine <sylvesterdivine@outlook.com>
2022-07-08 23:48:56 +0000

    task one, hotfix

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    lst = number % -10
+if number > 0:
+    lst = number % 10
+if lst > 5:
+    print(f"Last digit of {number} is {lst} and is greater than 5")
+elif lst < 6 and lst != 0:
+    print(f"Last digit of {number} is {lst} and is less than 6 and not 0")
+elif lst == 0:
+    print(f"Last digit of {number} is {lst} and is 0")
+
--- 0x01-python-if_else_loops_functions/run.sh
+++ 0x01-python-if_else_loops_functions/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m 'Zeroth task, hotfix'
+git commit -m 'task one, hotfix'
 git push
