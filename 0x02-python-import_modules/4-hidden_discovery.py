6468d99e975955cf126bf9d5cca833c3cacc702e
Sylvester_Divine <sylvesterdivine@outlook.com>
2022-07-29 22:00:23 +0000

    Hotfix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
-#!/bin/python3
-import hidden_4
-my_list = dir(hidden_4)
-my_list.sort()
-for x in my_list:
-    if x[0] == '_':
-        continue
-    else:
-        print("{}".format(x))
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    my_list = dir(hidden_4)
+    my_list.sort()
+    for x in my_list:
+        if x[0] == '_':
+            continue
+        else:
+            print("{}".format(x))
 
