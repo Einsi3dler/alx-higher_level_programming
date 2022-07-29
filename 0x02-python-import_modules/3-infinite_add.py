c50654905f910c1210ff1a9e122116b3657167f4
Sylvester_Divine <sylvesterdivine@outlook.com>
2022-07-29 21:49:34 +0000

    task 3 hotfix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,14 @@
-#!/bin/python3
-import sys
-addition = 0
-ind = 1
-my_list = (sys.argv)
-list_len = len((sys.argv))
-
-while (ind < list_len):
-    a = int(my_list[ind])
-    addition = addition + a
-    ind = ind + 1
-print("{}".format(addition))
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    addition = 0
+    ind = 1
+    my_list = (sys.argv)
+    list_len = len((sys.argv))
+    
+    while (ind < list_len):
+        a = int(my_list[ind])
+        addition = addition + a
+        ind = ind + 1
+    print("{}".format(addition))
 
