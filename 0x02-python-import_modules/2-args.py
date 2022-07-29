e9dc0b447da9c0845fa3d963f723b1e059fce29e
Sylvester_Divine <sylvesterdivine@outlook.com>
2022-07-29 21:45:29 +0000

    import modules hotfix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,14 @@ if __name__ == "__main__":
     import sys
     a = len((sys.argv))
     val = 1
-    my_list =  (sys.argv)
+    my_list = (sys.argv)
     if a == 1:
         print("{} arguments.".format(a-1))
+    elif a == 2:
+        print("{} argument:".format(a-1))
+        while val < a:
+            print("{}: {}".format(val, my_list[val]))
+            val = val+1
     else:
         print("{} arguments:".format(a-1))
         while val < a:
