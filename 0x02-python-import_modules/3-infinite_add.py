0c07159cfe701813083841f1c815d0a14109e3f3
Sylvester_Divine <sylvesterdivine@outlook.com>
2022-07-29 21:51:45 +0000

    import modules hotfix, pycode

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,10 +5,8 @@ if __name__ == "__main__":
     ind = 1
     my_list = (sys.argv)
     list_len = len((sys.argv))
-    
     while (ind < list_len):
         a = int(my_list[ind])
         addition = addition + a
         ind = ind + 1
     print("{}".format(addition))
-
