aba229270703c47a12ca6f575fb39219627b797d
sylvester_divine <sylvesterdivine@outlook.com>
2022-11-29 21:03:41 +0000

    6 and 5 refactor

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
     if num == 99:
-        print(f"{num:02}")
+        print("{0:0=2d}".format(num))
     else:
-        print(f"{num:02}", end=", ")
+        print("{0:0=2d}".format(num), end=", ")
