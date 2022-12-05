687a9b59d3fe663450ec61e3307d31d7d26c0602
sylvester_divine <sylvesterdivine@outlook.com>
2022-12-05 23:05:00 +0000

    task 7 fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,5 +4,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a, tuple_b = list(tuple_a), list(tuple_b)
     tuple_a.extend([0, 0])
     tuple_b.extend([0, 0])
-    final_res.extend([(tuple_a[0] + tuple_b[0]) , (tuple_a[1] + tuple_b[1])])
+    final_res.extend([(tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])])
     return (tuple(final_res))
