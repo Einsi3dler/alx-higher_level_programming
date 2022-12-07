811887c9323771c55ad7262046603981bc85a761
sylvester_divine <sylvesterdivine@outlook.com>
2022-12-07 20:16:20 +0000

    tee

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,8 +5,11 @@ def best_score(a_dictionary):
     elif len(a_dictionary.values()) < 0:
         return None
     else:
-        val = []
+        val = [0,0]
         for k,v in a_dictionary.items():
-            val.append(v)
-        val.sort()
-        return val[-1]
+            if v => val[1]:
+                val[0] = k
+                val[1] = v
+            else:
+                pass
+        return val[0]
