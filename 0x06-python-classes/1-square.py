6aac2efdd0c0bf22c32acf7b10ba4130b31c03b5
sylvester_divine <sylvesterdivine@outlook.com>
2022-12-20 22:35:13 +0000

    task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains a single class
+"""
+
+
+class Square:
+    """
+    This class contains a single function
+    """
+    def __init__(self, size):
+        """
+        This methods defines a private attribute called size
+        """
+        self.__size = size
