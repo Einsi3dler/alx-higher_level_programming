c479acc5c14194a2c08978931493c150874dd749
sylvester_divine <sylvesterdivine@outlook.com>
2022-12-20 23:26:31 +0000

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,8 +6,8 @@ This modules contains a single class
 
 class Square:
     """
-    This class contains a function
-    """ 
+    This class contains two function
+    """
     def __init__(self, size=0):
         """
         this initializes the square and validates the value
@@ -17,3 +17,10 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """
+        this is meant to calculate the general area of a square
+        """
+        val = self.__size
+        return val * val
