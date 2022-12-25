d29c11196dd0f99866f4fce3b8ffea5eb1e82d1f
sylvester_divine <sylvesterdivine@outlook.com>
2022-12-25 22:24:34 +0000

    file refix

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
     try:
         print(("{:d}").format(value))
         return True
-    except (TypeError, ValueError) as e:
+    except (TypeError, ValueError)as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
