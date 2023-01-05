b97465522cb4d71fa2260ff215a0c5201ff7f6c7
sylvester_divine <sylvesterdivine@outlook.com>
2023-01-05 00:26:13 +0000

    Task file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,9 +4,23 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
+    """
+    Test case class
+    """
     def test_value(self):
+        """
+        Function for tests case
+        """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
+        self.assertEqual(max_integer([1, 2, 5, 4, 3]), 5)
+        self.assertEqual(max_integer([4, 2, -3, 1]), 4)
+        self.assertEqual(max_integer([-4, -2, -3, -1]), -1)
+        self.assertEqual(max_integer([4]), 4)
+        self.assertIsNone(max_integer([]), "Test value is not none")
+
 
 if __name__ == '__main__':
     unittest.main()
