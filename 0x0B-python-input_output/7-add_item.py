f0630c76ef3efb37de8e2007ab52d2bdc6a749e9
sylvester_divine <sylvesterdivine@outlook.com>
2023-01-10 21:47:41 +0000

    file fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,9 +7,14 @@ import json
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-res = []
-for num in range(1, len(sys.argv)):
-    res.append(sys.argv[val])
-
-with open ("add_item.json", 'w+', enconding='utf8'):
-    load_from_json_file()
+f_nm = "add_item.json"
+flag = 0
+try:
+    data = load_from_json_file(f_nm)
+    for val in sys.argv:
+        flag += 1
+        if flag != 1:
+            data.append(val)
+    save_to_json_file(data, f_nm)
+except Exception:
+    save_to_json_file(sys.argv[1:], f_nm)
