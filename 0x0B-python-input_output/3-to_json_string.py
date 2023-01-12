7416157fa3a11ac1a581416628337ff2aab94839
Einsi3dler <102097200+Einsi3dler@users.noreply.github.com>
2023-01-12 23:54:16 +0100

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,4 +4,7 @@ This module contains a function interacting with json module
 """
 import json
 def to_json_string(my_obj):
+    """
+    This file converts objs to strings
+    """
     return json.dumps(my_obj)
