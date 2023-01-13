00ed6c9637712224b95f78dc2cdf01fa3342713b
sylvester_divine <sylvesterdivine@outlook.com>
2023-01-13 16:05:35 +0000

    file commit

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This module contains a single class
+"""
+
+
+class Student():
+    """
+    This class represents a JSON storage structure
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialization function
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This json func intializes the storage structure
+        """
+        if attrs is None:
+            data = sorted(self.__dict__)
+            res = {}
+            for val in data:
+                res[val] = self.__dict__[val]
+            return res
+        else:
+            data = self.__dict__
+            res = {}
+            arr = sorted(attrs)
+            for val in arr:
+                if val in data.keys():
+                    res[val] = data[val]
+                else:
+                    pass
+            return res
+
+    def reload_from_json(self, json):
+        """
+        documentation
+        """
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
