eb5e2310bb2947568e7a67c6e46bf48e2f3db943
sylvester_divine <sylvesterdivine@outlook.com>
2023-01-13 22:37:10 +0000

    advance task

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def append_after(filename="", search_string="", new_string=""):
+    """
+    This searches a file for a str and place a new str undee
+    """
+    lines = []
+    with open(filename, 'r') as f:
+        contents = f.readlines()
+        for index, line in enumerate(contents):
+            if search_string in line:
+                lines.append(index)
+    flg = 1
+    for num in lines:
+        contents.insert(num+flg, f"{new_string}")
+        flg = flg + 1
+    with open(filename, 'a') as f:
+        f.writelines(contents)
