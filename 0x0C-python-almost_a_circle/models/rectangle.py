47991773ea910302f6fd050b4200cfbd392b0a8f
sylvester_divine <sylvesterdivine@outlook.com>
2023-01-14 22:11:05 +0000

    task 2 +

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,3 +6,47 @@ This module contains a single class
 
 from models.base import Base
 class Rectangle(Base):
+    """
+    This class defines a rectangle and inherits id from a base class
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        intialization, id is from base(parent class) btw
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super(Rectangle, self).__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
