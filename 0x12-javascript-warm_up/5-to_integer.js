1575714a79a0acf2b8673d0ddae8dd120bf1ea11
sylvester_divine <sylvesterdivine@outlook.com>
2023-03-17 12:57:55 +0000

    file add

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const num = process.argv;
 if (!isNaN(num[2])) {
-  console.log('My number:' + parseInt(num[2]));
+  console.log('My number: ' + parseInt(num[2]));
 } else {
   console.log('Not a number');
 }
