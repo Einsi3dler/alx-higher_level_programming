7c6f9ccca402ba8950874e9a0070f06e8c416808
Sylvester_Divine <sylvesterdivine@outlook.com>
2023-03-21 00:00:40 +0000

    pep8 linter fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+displays all values in the states table \
+        of hbtn_0e_0_usa where name matches the argument.
 """
 import MySQLdb
 from sys import argv
@@ -9,7 +10,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * from states WHERE name='{}' ORDER BY id ASC".format(argv[4]))
+    cursor.execute("SELECT * from states WHERE name='{}'
+                   ORDER BY id ASC".format(argv[4]))
     rows = cursor.fetchall()
     for row in rows:
         if row[1] == argv[4]:
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.state_id = states.id ORDER BY cities.id ASC")
+    cursor.execute("SELECT cities.id, cities.name, states.name
+                   FROM cities, states
+                   WHERE cities.state_id=states.id ORDER BY cities.id ASC")
     query_rows = cursor.fetchall()
 
     for row in query_rows:
