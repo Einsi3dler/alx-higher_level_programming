f05e4c6a0b2bba13a132a829a1628eb388a4a111
Sylvester_Divine <sylvesterdivine@outlook.com>
2023-03-31 02:05:32 +0000

    task 0 file refix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-str=$(curl -s "$1")
-echo ${#str}
+cur -sI "$1" |grep "Content-Length"| awk -F ":" 'print $2'
