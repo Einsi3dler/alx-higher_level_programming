fdcf3a53cd658a40f12e93f1dbc22a06b90c2756
sylvester_divine <sylvesterdivine@outlook.com>
2023-04-02 20:29:12 +0000

    white space refix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -siLX OPTIONS "$1" | grep "Allow" | awk -F ':' '{print $2}'
+curl -siLX OPTIONS "$1" | grep "Allow" | awk -F ': ' '{print $2}'
